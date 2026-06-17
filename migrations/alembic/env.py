e60e0e2b6756a1367924b865185e772879fa4582
mehmetcagriekici <mehmetcagriekici@gmail.com>
2026-06-17 16:51:00 +0300

    local postgres database

--- migrations/alembic/env.py
+++ migrations/alembic/env.py
@@ -5,11 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-import os
-from dotenv import load_dotenv
+from pathlib import Path
+import sys
 
-# load environmet variables
-load_dotenv()
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
+
+from models.base import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,18 +26,12 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-# db url
-db_url = os.getenv("database_url")
-if db_url:
-    config.set_main_option("sqlalchemy.url", db_url)
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from models.base import Base
+from models.user import User
+from models.document import Document
+
+__all__ = ["Base", "User", "Document"]
